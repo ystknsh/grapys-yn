cfc6c481bfd4afb8857480edbe651fc9b1521aa7
isamu <isamu@to-kyo.to>
2025-02-28 20:55:35 +0900

    more

--- packages/grapys-vue/src/graph/index.ts
+++ packages/grapys-vue/src/graph/index.ts
@@ -5,6 +5,7 @@ import { graphData as graphDataReducerPush } from "../graph/reducer_push";
 import { graphData as graphDataReducerPop } from "../graph/reducer_pop";
 import { graphData as graphDataTools } from "../graph/tools1";
 import { graphData as graphDataStreamTest } from "../graph/stream_test";
+import { graphData as graphDataStreamTest2 } from "../graph/stream_test2";
 
 export const graphs = [
   { name: "Chat(WebLLM)", graph: graphChatTinySwallow },
@@ -14,6 +15,7 @@ export const graphs = [
   { name: "Reducer(pop)", graph: graphDataReducerPop },
   { name: "Tools", graph: graphDataTools },
   { name: "StreamTest", graph: graphDataStreamTest },
+  { name: "StreamTest2", graph: graphDataStreamTest2 },
 ];
 
 export const graphConfigs = {
---
+++ packages/grapys-vue/src/graph/stream_test2.ts
@@ -0,0 +1,445 @@
+export const graphData = {
+  metadata: {
+    data: {
+      nodes: [
+        {
+          data: {
+            value: "this is streaming test",
+            staticNodeType: "text",
+          },
+          nodeId: "test",
+          type: "static",
+          position: {
+            x: 190.66406034756648,
+            y: 52.92399307959997,
+            width: 143.99305725097656,
+            height: 224.29689025878906,
+            inputCenters: [55.95485973358154],
+            outputCenters: [35.97222423553467],
+          },
+        },
+        {
+          data: {
+            agent: "streamMockAgent",
+            guiAgentId: "streamMockAgent",
+            params: {
+              stream: true,
+              isResult: true,
+            },
+          },
+          nodeId: "stream",
+          type: "computed",
+          position: {
+            x: 395.25693835672416,
+            y: 25.570546630608703,
+            width: 143.99305725097656,
+            height: 314.0538330078125,
+            inputCenters: [75.93750286102295],
+            outputCenters: [55.95485973358154],
+          },
+        },
+        {
+          data: {
+            value: "hello world.",
+            staticNodeType: "text",
+          },
+          nodeId: "test2",
+          type: "static",
+          position: {
+            x: 30.805477466806792,
+            y: 365.58810297237505,
+            width: 143.9930419921875,
+            height: 224.29689025878906,
+            inputCenters: [55.954874992370605],
+            outputCenters: [35.97223949432373],
+          },
+        },
+        {
+          data: {
+            value: "pen pineapple apple pen.",
+            staticNodeType: "text",
+          },
+          nodeId: "test3",
+          type: "static",
+          position: {
+            x: 46.20569295832968,
+            y: 647.2047178164688,
+            width: 143.99305725097656,
+            height: 224.29689025878906,
+            inputCenters: [55.954813957214355],
+            outputCenters: [35.972208976745605],
+          },
+        },
+        {
+          data: {
+            agent: "streamMockAgent",
+            guiAgentId: "streamMockAgent",
+            params: {
+              stream: true,
+              isResult: true,
+            },
+          },
+          nodeId: "stream2",
+          type: "computed",
+          position: {
+            x: 392.6919781212091,
+            y: 363.4295405050342,
+            width: 143.99305725097656,
+            height: 314.0538024902344,
+            inputCenters: [75.93751049041748],
+            outputCenters: [55.954874992370605],
+          },
+        },
+        {
+          data: {
+            agent: "streamMockAgent",
+            guiAgentId: "streamMockAgent",
+            params: {
+              stream: true,
+              isResult: true,
+            },
+          },
+          nodeId: "stream3",
+          type: "computed",
+          position: {
+            x: 426.5374377798934,
+            y: 690.6786483375371,
+            width: 143.99305725097656,
+            height: 314.0538330078125,
+            inputCenters: [75.9375410079956],
+            outputCenters: [55.954874992370605],
+          },
+        },
+        {
+          data: {
+            agent: "streamMockAgent",
+            guiAgentId: "streamMockAgent",
+            params: {
+              stream: true,
+              isResult: true,
+            },
+          },
+          nodeId: "2",
+          type: "computed",
+          position: {
+            x: 602.11004876551,
+            y: 34.24809034457908,
+            width: 143.99305725097656,
+            height: 314.0538330078125,
+            inputCenters: [75.93750619888306],
+            outputCenters: [55.954867362976074],
+          },
+        },
+        {
+          data: {
+            agent: "streamMockAgent",
+            guiAgentId: "streamMockAgent",
+            params: {
+              stream: true,
+              isResult: true,
+            },
+          },
+          nodeId: "3",
+          type: "computed",
+          position: {
+            x: 867.6882586078789,
+            y: 767.9723801649063,
+            width: 143.99305725097656,
+            height: 314.0538330078125,
+            inputCenters: [75.9375410079956],
+            outputCenters: [55.954874992370605],
+          },
+        },
+        {
+          data: {
+            agent: "streamMockAgent",
+            guiAgentId: "streamMockAgent",
+            params: {
+              stream: true,
+              isResult: true,
+            },
+          },
+          nodeId: "4",
+          type: "computed",
+          position: {
+            x: 640.3052289517019,
+            y: 766.3809518697419,
+            width: 143.99305725097656,
+            height: 314.0538330078125,
+            inputCenters: [75.93747997283936],
+            outputCenters: [55.954874992370605],
+          },
+        },
+        {
+          data: {
+            agent: "streamMockAgent",
+            guiAgentId: "streamMockAgent",
+            params: {
+              stream: true,
+              isResult: true,
+            },
+          },
+          nodeId: "5",
+          type: "computed",
+          position: {
+            x: 1113.9305010953344,
+            y: 344.19992229720015,
+            width: 143.99305725097656,
+            height: 314.0538330078125,
+            inputCenters: [75.93751049041748],
+            outputCenters: [55.954874992370605],
+          },
+        },
+        {
+          data: {
+            agent: "streamMockAgent",
+            guiAgentId: "streamMockAgent",
+            params: {
+              stream: true,
+              isResult: true,
+            },
+          },
+          nodeId: "6",
+          type: "computed",
+          position: {
+            x: 631.7112300905428,
+            y: 406.59404600851894,
+            width: 143.99305725097656,
+            height: 314.0538330078125,
+            inputCenters: [75.93747997283936],
+            outputCenters: [55.95484447479248],
+          },
+        },
+        {
+          data: {
+            agent: "streamMockAgent",
+            guiAgentId: "streamMockAgent",
+            params: {
+              stream: true,
+              isResult: true,
+            },
+          },
+          nodeId: "7",
+          type: "computed",
+          position: {
+            x: 1116.1208853097933,
+            y: 1.1855661160355737,
+            width: 143.99305725097656,
+            height: 314.0538330078125,
+            inputCenters: [75.93750476837158],
+            outputCenters: [55.954861640930176],
+          },
+        },
+        {
+          data: {
+            agent: "streamMockAgent",
+            guiAgentId: "streamMockAgent",
+            params: {
+              stream: true,
+              isResult: true,
+            },
+          },
+          nodeId: "8",
+          type: "computed",
+          position: {
+            x: 888.9868533671468,
+            y: 59.94356289248634,
+            width: 143.99305725097656,
+            height: 314.0538330078125,
+            inputCenters: [75.93750286102295],
+            outputCenters: [55.954867362976074],
+          },
+        },
+        {
+          data: {
+            agent: "streamMockAgent",
+            guiAgentId: "streamMockAgent",
+            params: {
+              stream: true,
+              isResult: true,
+            },
+          },
+          nodeId: "9",
+          type: "computed",
+          position: {
+            x: 854.773443728303,
+            y: 415.727047032778,
+            width: 143.99313354492188,
+            height: 314.0538024902344,
+            inputCenters: [75.93747997283936],
+            outputCenters: [55.95484447479248],
+          },
+        },
+        {
+          data: {
+            agent: "streamMockAgent",
+            guiAgentId: "streamMockAgent",
+            params: {
+              stream: true,
+              isResult: true,
+            },
+          },
+          nodeId: "1",
+          type: "computed",
+          position: {
+            x: 1075.758264041538,
+            y: 711.6279760119644,
+            width: 143.99305725097656,
+            height: 314.0538330078125,
+            inputCenters: [75.93747997283936],
+            outputCenters: [55.954813957214355],
+          },
+        },
+      ],
+      edges: [
+        {
+          type: "edge",
+          source: {
+            nodeId: "test",
+            index: 0,
+          },
+          target: {
+            nodeId: "stream",
+            index: 0,
+            direction: "outbound",
+          },
+        },
+        {
+          type: "edge",
+          source: {
+            nodeId: "test2",
+            index: 0,
+          },
+          target: {
+            nodeId: "stream2",
+            index: 0,
+            direction: "outbound",
+          },
+        },
+        {
+          type: "edge",
+          source: {
+            nodeId: "test3",
+            index: 0,
+          },
+          target: {
+            nodeId: "stream3",
+            index: 0,
+            direction: "outbound",
+          },
+        },
+        {
+          type: "edge",
+          source: {
+            nodeId: "stream",
+            index: 0,
+          },
+          target: {
+            nodeId: "2",
+            index: 0,
+            direction: "outbound",
+          },
+        },
+        {
+          type: "edge",
+          source: {
+            nodeId: "stream2",
+            index: 0,
+          },
+          target: {
+            nodeId: "6",
+            index: 0,
+            direction: "outbound",
+          },
+        },
+        {
+          type: "edge",
+          source: {
+            nodeId: "stream3",
+            index: 0,
+          },
+          target: {
+            nodeId: "4",
+            index: 0,
+            direction: "outbound",
+          },
+        },
+        {
+          type: "edge",
+          source: {
+            nodeId: "6",
+            index: 0,
+          },
+          target: {
+            nodeId: "9",
+            index: 0,
+            direction: "outbound",
+          },
+        },
+        {
+          type: "edge",
+          source: {
+            nodeId: "2",
+            index: 0,
+          },
+          target: {
+            nodeId: "8",
+            index: 0,
+            direction: "outbound",
+          },
+        },
+        {
+          type: "edge",
+          source: {
+            nodeId: "8",
+            index: 0,
+          },
+          target: {
+            nodeId: "7",
+            index: 0,
+            direction: "outbound",
+          },
+        },
+        {
+          type: "edge",
+          source: {
+            nodeId: "9",
+            index: 0,
+          },
+          target: {
+            nodeId: "5",
+            index: 0,
+            direction: "outbound",
+          },
+        },
+        {
+          type: "edge",
+          source: {
+            nodeId: "4",
+            index: 0,
+          },
+          target: {
+            nodeId: "3",
+            index: 0,
+            direction: "outbound",
+          },
+        },
+        {
+          type: "edge",
+          source: {
+            nodeId: "3",
+            index: 0,
+          },
+          target: {
+            nodeId: "1",
+            index: 0,
+            direction: "outbound",
+          },
+        },
+      ],
+      loop: {
+        loopType: "none",
+      },
+    },
+  },
+};
