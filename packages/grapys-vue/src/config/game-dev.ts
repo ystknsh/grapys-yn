0dfc01d4de1fc7bc7d4f0f84bc86cb3b008b85f6
isamu <isamu@to-kyo.to>
2025-03-28 06:20:32 +0900

    add firebase

---
+++ packages/grapys-vue/src/config/game-dev.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBLexhfLknmQxmuxFJC1v5t1-CkbGgwH-4",
+  authDomain: "game-graphai-dev.firebaseapp.com",
+  projectId: "game-graphai-dev",
+  storageBucket: "game-graphai-dev.firebasestorage.app",
+  messagingSenderId: "185975114870",
+  appId: "1:185975114870:web:dc182434167838b326a357",
+  measurementId: "G-117TSJ84PZ"
+};
---
+++ packages/grapys-vue/src/config/game.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyB-_EOLnz65LSjXMoDTyHCp1ybkvmgDxs4",
+  authDomain: "game-graphai.firebaseapp.com",
+  projectId: "game-graphai",
+  storageBucket: "game-graphai.firebasestorage.app",
+  messagingSenderId: "427990278188",
+  appId: "1:427990278188:web:0269a6cd684f5997d13189",
+  measurementId: "G-DRQX8W22KB"
+};
