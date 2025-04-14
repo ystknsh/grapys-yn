5d25e62077d73af2e9500463a91db5f29c3fbf80
isamu <isamu@to-kyo.to>
2025-04-14 10:01:39 +0900

    component

--- packages/grapys-vue/src/components/Signin.vue
+++ packages/grapys-vue/src/components/Signin.vue
@@ -10,22 +10,6 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import { googleSignin } from "../utils/firebase/SocialLogin";
-
-export default defineComponent({
-  name: "AccountPage",
-  setup() {
-    return {
-      googleSignin: googleSignin(
-        () => {
-          console.log("success");
-        },
-        (err) => {
-          console.log(err.message);
-        },
-      ),
-    };
-  },
-});
+import signin from "./signin";
+export default signin;
 </script>
