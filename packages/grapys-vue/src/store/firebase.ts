aef1bb5dc9472beb62945004f91026c9fd84ffb7
isamu <isamu@to-kyo.to>
2025-04-16 06:38:31 +0900

    fix

--- packages/grapys-vue/src/store/firebase.ts
+++ packages/grapys-vue/src/store/firebase.ts
@@ -13,7 +13,7 @@ export const useFirebaseStore = defineStore("firebase", () => {
     if (firebaseUser.value === null) {
       return null;
     }
-    return !!firebaseUser.value;
+    return Boolean(firebaseUser.value);
   });
 
   return {
