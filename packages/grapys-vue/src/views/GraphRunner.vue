388d2bc97a3e67958adcf74acdda943a0fa5717c
norihirosunada <norisuna1201@gmail.com>
2025-05-05 17:11:45 +0900

    Refactor event handling

--- packages/grapys-vue/src/views/GraphRunner.vue
+++ packages/grapys-vue/src/views/GraphRunner.vue
@@ -4,7 +4,11 @@
     <div class="flex cursor-pointer items-center justify-between rounded-t-lg border border-gray-300 bg-gray-100 p-3">
       <div class="flex items-center font-bold">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 0 1 .865-.501 48.172 48.172 0 0 0 3.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z" />
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 0 1 .865-.501 48.172 48.172 0 0 0 3.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z"
+          />
         </svg>
         <span>Chat</span>
         <span v-if="messages.length > 0" class="ml-2 rounded-full bg-blue-500 px-2 py-1 text-xs text-white">
@@ -28,7 +32,7 @@
         >
           Stop
         </button>
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6" @click="handleClick">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6" @click="onClickClose">
           <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
         </svg>
       </div>
@@ -123,12 +127,13 @@ export default defineComponent({
       type: Object as PropType<GraphData>,
       required: true,
     },
-    onClick: {
-      type: Function,
-      default: null,
+    isOpen: {
+      type: Boolean,
+      default: false,
     },
   },
-  setup(props) {
+  emits: ["close"],
+  setup(props, { emit }) {
     const isRunning = ref(false);
     const chatContainerRef = ref<HTMLElement | null>(null);
     const store = useStore();
@@ -244,10 +249,8 @@ export default defineComponent({
       }
     };
 
-    const handleClick = () => {
-      if (props.onClick) {
-        props.onClick();
-      }
+    const onClickClose = () => {
+      emit("close");
     };
 
     return {
@@ -265,7 +268,7 @@ export default defineComponent({
       events,
       streamNodes,
       handleKeyPress,
-      handleClick,
+      onClickClose,
       loading,
       ready,
     };
--- packages/grapys-vue/src/views/JsonViewer.vue
+++ packages/grapys-vue/src/views/JsonViewer.vue
@@ -1,5 +1,6 @@
 <script lang="ts">
 import { defineComponent } from "vue";
+
 export default defineComponent({
   name: "JsonViewer",
   props: {
@@ -7,20 +8,19 @@ export default defineComponent({
       type: Object,
       required: true,
     },
-    onClick: {
-      type: Function,
-      default: null,
+    isOpen: {
+      type: Boolean,
+      default: false,
     },
   },
-  setup(props) {
-    const handleClick = () => {
-      if (props.onClick) {
-        props.onClick();
-      }
+  emits: ["close"],
+  setup(props, { emit }) {
+    const onClickClose = () => {
+      emit("close");
     };
 
     return {
-      handleClick,
+      onClickClose,
     };
   },
 });
@@ -32,11 +32,15 @@ export default defineComponent({
     <div class="flex cursor-pointer items-center justify-between rounded-t-lg border border-gray-300 bg-gray-100 p-3 text-black">
       <div class="flex items-center font-bold">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M14.25 9.75 16.5 12l-2.25 2.25m-4.5 0L7.5 12l2.25-2.25M6 20.25h12A2.25 2.25 0 0 0 20.25 18V6A2.25 2.25 0 0 0 18 3.75H6A2.25 2.25 0 0 0 3.75 6v12A2.25 2.25 0 0 0 6 20.25Z" />
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M14.25 9.75 16.5 12l-2.25 2.25m-4.5 0L7.5 12l2.25-2.25M6 20.25h12A2.25 2.25 0 0 0 20.25 18V6A2.25 2.25 0 0 0 18 3.75H6A2.25 2.25 0 0 0 3.75 6v12A2.25 2.25 0 0 0 6 20.25Z"
+          />
         </svg>
         <span>JSON Data</span>
       </div>
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6" @click="handleClick">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6" @click="onClickClose">
         <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
       </svg>
     </div>
