98c5a34c5e2296bc546e9bbfb509a33ec9b3d6d3
isamu <isamu@to-kyo.to>
2025-06-19 14:56:55 +0900

    update

--- packages/grapys-vue/src/utils/gui/type.ts
+++ packages/grapys-vue/src/utils/gui/type.ts
@@ -121,7 +121,7 @@ export type NearestData = {
 export type InputOutputType = "text" | "array" | "message" | "data" | "wait";
 export type InputOutputData = { name: string; type?: InputOutputType, mapTo?: string };
 
-export type ParamType = "string" | "text" | "data" | "boolean" | "float" | "int" | "enum";
+export type ParamType = "string" | "text" | "data" | "boolean" | "float" | "int" | "enum" | "boolean";
 
 export type ParamData = {
   name: string;
